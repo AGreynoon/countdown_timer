ce8f9743c2123f59ef97b28c5289ad4098f4f107
AGreynoon <a.greynoon@bootfi.com>
2026-03-24 15:21:03 +0300

    cteate pages

---
+++ lib/add_countdown_timer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddCountdownTimer extends StatelessWidget {
+  const AddCountdownTimer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/countdown_timer_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CountdownTimerDetails extends StatelessWidget {
+  const CountdownTimerDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
